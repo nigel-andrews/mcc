697ba2fe1be037af17438dc4d2c8c967e727b25b
Nigel Andrews <nigel.andrews@epita.fr>
2024-04-13 11:33:27 +0200

    fix(lexer): recognize semi and ws

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <format>
 #include <fstream>
 #include <iostream>
 
@@ -7,14 +8,18 @@ int main(int argc, char** argv)
 {
     if (argc < 2)
     {
-        std::cerr << "Usage : mcc <source C file>";
+        std::cerr << "Usage : mcc <source C file>\n";
         return 1;
     }
 
     auto source = std::ifstream(argv[1]);
+    if (!source)
+    {
+        std::cerr << std::format("Unable to open file : {}\n", argv[1]);
+    }
     reflex::Input lexer_input(source);
 
-    auto ret = Lexer(lexer_input).lex();
+    auto ret = Lexer(lexer_input, std::cout).lex();
 
     return ret;
 }
