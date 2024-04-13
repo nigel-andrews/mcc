9a0ecd2245028b1214e11494ec9948388a240969
Nigel Andrews <nigel.andrews@epita.fr>
2024-04-13 09:31:18 +0200

    feat: read lexer input from source file

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,20 @@
+#include <fstream>
 #include <iostream>
 
 #include "parser/lexer.h"
 
-int main()
+int main(int argc, char** argv)
 {
-    return Lexer().lex();
+    if (argc < 2)
+    {
+        std::cerr << "Usage : mcc <source C file>";
+        return 1;
+    }
+
+    auto source = std::ifstream(argv[1]);
+    reflex::Input lexer_input(source);
+
+    auto ret = Lexer(lexer_input).lex();
+
+    return ret;
 }
