ae0b527f7070a59c03bf087c34e9c421f2f6ae8b
Nigel Andrews <nigel.andrews@epita.fr>
2024-05-02 19:13:16 +0200

    chore: update flake

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
           gnumake
           bison
           buildenv.outputs.packages.${system}.reflex
-          flex # just in case
+          llvmPackages_17.libcxx
         ];
       };
     };
