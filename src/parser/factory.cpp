c03bbfb5d13d04bbe0baf7d78c283679f49b1572
Ethan Zouzoulkowsky <ethan.zouzoulkowsky@gmail.com>
2025-03-13 17:43:46 +0100

    chore(factory): returning actual classes instead of Nodes

--- src/parser/factory.cpp
+++ src/parser/factory.cpp
@@ -6,17 +6,17 @@
 
 namespace ast::factory
 {
-    Node* make_int_expr(const parse::location& location, int val)
+    IntExpr* make_int_expr(const parse::location& location, int val)
     {
         return new IntExpr(location, val);
     }
 
-    Node* make_fundec(const parse::location& location, std::string_view name)
+    FunDec* make_fundec(const parse::location& location, std::string_view name)
     {
         return new FunDec(location, name);
     }
 
-    Node* make_return_stmt(const parse::location& location, Expr expr)
+    ReturnStmt* make_return_stmt(const parse::location& location, Expr expr)
     {
         return new ReturnStmt(location, std::move(expr));
     }
--- src/parser/factory.h
+++ src/parser/factory.h
@@ -11,7 +11,7 @@ namespace parse
 
 namespace ast::factory
 {
-    Node* make_int_expr(const parse::location& location, int val);
-    Node* make_fundec(const parse::location& location, std::string_view name);
-    Node* make_return_stmt(const parse::location& location, Expr* expr);
+    IntExpr* make_int_expr(const parse::location& location, int val);
+    FunDec* make_fundec(const parse::location& location, std::string_view name);
+    ReturnStmt* make_return_stmt(const parse::location& location, Expr* expr);
 } // namespace ast::factory
